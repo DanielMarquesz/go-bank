836abf848fc3e8cfd54f2d837f997fb9c59ddaad
Daniel Marques <daniel.marques142@gmail.com>
2023-09-23 20:49:14 -0300

    feat: mock get account route

--- api.go
+++ api.go
@@ -43,6 +43,7 @@ func (s *APIServer) Run() {
 	router := mux.NewRouter()
 
 	router.HandleFunc("/account", makeHTTPHandleFunc(s.handleAccount))
+	router.HandleFunc("/account/{id}", makeHTTPHandleFunc(s.handleAccount))
 
 	log.Println("JSON API server running on port: ", s.listenAddr)
 
@@ -66,9 +67,13 @@ func (s *APIServer) handleAccount(w http.ResponseWriter, r *http.Request) error 
 }
 
 // Adicionar Update Account
-
 func (s *APIServer) handleGetAccount(w http.ResponseWriter, r *http.Request) error {
-	return nil
+	// account := NewAccount("Daniel", "Marques")
+	id := mux.Vars(r)["id"]
+
+	fmt.Println(id)
+
+	return WriteJSON(w, http.StatusOK, &Account{})
 }
 
 func (s *APIServer) handleCreateAccount(w http.ResponseWriter, r *http.Request) error {
